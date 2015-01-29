77d897efd61d9ac426c3a1b568d70528f8d78ffa
Bradyn Poulsen <bradyn@bradynpoulsen.com>
2015-01-28 18:26:25 -0700

    add jsdoc

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,5 +1,6 @@
 /**
  * Tic Tac Toe
+ * @license MIT
  * Copyright (c) 2015-2018 Bradyn Poulsen
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -36,14 +37,30 @@
     ['square02', 'square11', 'square20']
   ];
 
+  /**
+   * TicTacToe
+   * @class
+   *
+   * @param {TicTacToe~gameOver`} done - Game over callback
+   */
   var TicTacToe = function(done){
+    /** @member {boolean} */
     this.active = true;
+    /** @member {number} */
     this.turn = 0;
 
+    /**
+     * @method takeTurn
+     * @description handle the next turn using element
+     *
+     * @param {HTMLElement} element
+     * @return {undefined}
+     */
     this.takeTurn = function(element){
       if(!this.active || element.innerHTML !== '&nbsp;') return;
       this.turn++;
       
+      /** @type {string} */
       element.innerHTML = (this.turn % 2) ? 'X' : 'O';
       this.checkWin(element);
 
@@ -53,6 +70,14 @@
       }
     };
 
+    /**
+     * @method checkWin
+     * @description check if there is a winning combo. If there is, highlight combos
+     *  and notify the winner.
+     *
+     * @param {HTMLElement} element
+     * @return {undefined}
+     */
     this.checkWin = function(element){
       var won = false;
       this.eachCombo(element.id, function(group){
@@ -68,6 +93,14 @@
       }
     };
 
+    /**
+     * @method eachCombo
+     * @description filter possible win combos that contain `id` and execute `next`
+     *
+     * @param {string} id - element id to filter by
+     * @param {TicTacToe~comboGroup} next - function to call on each result
+     * @return {undefined}
+     */
     this.eachCombo = function(id, next){
       var i;
       for(i = 0; i < wins.length; i++){
@@ -75,9 +108,17 @@
       }
     };
 
+    /**
+     * @method groupSingleOwner
+     * @description verify that all elements in combo group have same owner
+     *
+     * @param {string[]} group
+     * @return {boolean}
+     */
     this.groupSingleOwner = function(group){
       var i, owner, squareOwner;
       for(i = 0; i < group.length; i++){
+        /** @type {string} */
         squareOwner = this.getSquare(group[i]).innerHTML;
         if(!owner){
           owner = squareOwner;
@@ -88,6 +129,13 @@
       return true;
     };
 
+    /**
+     * @method highlightGroup
+     * @description apply 'winner' class to each element in combo group
+     *
+     * @param {string[]} group
+     * @return {undefined}
+     */
     this.highlightGroup = function(group){
       var i;
       for(i = 0; i < group.length; i++){
@@ -95,26 +143,57 @@
       }
     };
 
+    /**
+     * @method notifyWinner
+     * @description alert the game winner and fire the {@link TicTacToe~gameOver}
+     *
+     * @param {string=} team
+     * @return {undefined}
+     */
     this.notifyWinner = function(team){
       if(team) alert('Player '+team+' won!');
       else alert('It was a tie game!');
       if(done) done.call(this, team || 'tie');
     };
 
+    /**
+     * @method getSquare
+     * @description perform a getElementById lookup
+     *
+     * @param {string} id
+     * @return {HTMLElement}
+     */
     this.getSquare = function(id){
       return document.getElementById(id);
     };
+
+    /**
+     * @callback TicTacToe~gameOver
+     * @this TicTacToe
+     * @param {string} [team='tie']
+     */
+
+    /**
+     * @callback TicTacToe~comboGroup
+     * @param {string[]} group
+     */
   };
 
+  /** @type {TicTacToe} */
   var game = new TicTacToe(function(){
-    // callback for when the game is over
     setTimeout(function(){
        location.reload();
     }, 1000);
   });
   
+  /**
+   * @function handleClick
+   * @description click event handler used to avoid binding conflicts
+   *
+   * @this HTMLElement
+   * @return {undefined}
+   */
   var handleClick = function(){
-    // use a separate function because of bindings
     game.takeTurn(this);
   }, e, x, y;
   for(e = 0; e < 9; e++){
