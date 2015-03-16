4b87bf3b76eeb603eb7ad6d5c6299ca0af7bcd96
Bradyn Poulsen <bradyn@bradynpoulsen.com>
2015-03-16 08:54:30 -0600

    state store

---
+++ gallery/application.js
@@ -0,0 +1,16 @@
+(function(){
+  var Application = function(){};
+  Application.prototype.controller = function(name, dependencies){
+
+  };
+  
+
+  var app = new Application();
+
+
+
+  var ready = (function(){
+    
+  }).bind(this);
+  this.addEventListener("load", ready, false);
+}).call(this);
